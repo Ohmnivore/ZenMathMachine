d73deeae18451f3be2d5855a6b19bd9b09d8359b
Ohmnivore <markbeiline@gmail.com>
2016-05-14 09:54:07 -0400

    Added difficulty to division too

--- source/modes/Division.hx
+++ source/modes/Division.hx
@@ -12,18 +12,62 @@ class Division extends BaseMode {
 	}
 	
 	override private function getOperands():Array<Int> {
-		var n1 = getOperand();
-		var n2 = getOperand();
-		answer = n1;
-		n1 = n1 * n2;
-		return [n1, n2];
+		if (Reg.difficulty == 0) {
+			var n1:Int;
+			var n2:Int;
+			do {
+				n1 = getOperand(1);
+				n2 = getOperand(1);
+			} while (n1 == n1 * n2 || n2 == n1 * n2 || n1 * n2 > 20);
+			answer = n1;
+			return [n1 * n2, n2];
+		}
+		else if (Reg.difficulty == 1) {
+			var n1:Int;
+			var n2:Int;
+			do {
+				n1 = getOperand(FlxRandom.intRanged(1, 2));
+				n2 = getOperand(FlxRandom.intRanged(1, 2));
+			} while (n1 == n1 * n2 || n2 == n1 * n2 || n1 * n2 > 100);
+			answer = n1;
+			return [n1 * n2, n2];
+		}
+		else if (Reg.difficulty == 2) {
+			var n1:Int;
+			var n2:Int;
+			do {
+				n1 = getOperand(FlxRandom.intRanged(1, 2));
+				n2 = getOperand(FlxRandom.intRanged(1, 2));
+			} while (n1 == n1 * n2 || n2 == n1 * n2 || n1 * n2 > 200);
+			answer = n1;
+			return [n1 * n2, n2];
+		}
+		else if (Reg.difficulty == 3) {
+			var n1:Int;
+			var n2:Int;
+			do {
+				n1 = getOperand(2);
+				n2 = getOperand(FlxRandom.intRanged(1, 2));
+			} while (n1 == n1 * n2 || n2 == n1 * n2 || n1 * n2 > 1000);
+			answer = n1;
+			return [n1 * n2, n2];
+		}
+		else {
+			var n1:Int;
+			var n2:Int;
+			do {
+				n1 = getOperand(2);
+				n2 = getOperand(2);
+			} while (n1 == n1 * n2 || n2 == n1 * n2);
+			answer = n1;
+			return [n1 * n2, n2];
+		}
 	}
 	
-	private function getOperand():Int {
-		var digits:Int = FlxRandom.intRanged(1, 2);
-		if (digits == 1)
+	private function getOperand(Digits:Int):Int {
+		if (Digits == 1)
 			return FlxRandom.intRanged(1, 9);
-		else if (digits == 2)
+		else if (Digits == 2)
 			return FlxRandom.intRanged(10, 99);
 		else
 			return FlxRandom.intRanged(100, 999);
